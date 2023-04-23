67ab6779e0ff718e190e7924cf31c3b677dab95e
Воронков Вадим <vaadimvoronkov@gmail.com>
2023-04-23 12:20:03 +0700

    Добавление примерного наброска ViewModel для основного окна и для окна информации

---
+++ CourseWork2/ViewModel/InformationOnDateVM.cs
@@ -0,0 +1,47 @@
+﻿using CourseWork2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork2.ViewModel
+{
+    public class InformationOnDateVM: INotifyPropertyChanged
+    {
+        private Lesson _lesson;
+        public string LessonName
+        {
+            get
+            {
+                return _lesson.LessonName;
+            }
+            set
+            {
+                _lesson.LessonName = value;
+                OnPropertyChanged("ChangedName");
+            }
+        }
+        public string LessonTask
+        {
+            get
+            {
+                return _lesson.Task;
+            }
+            set
+            {
+                _lesson.Task = value;
+                OnPropertyChanged("ChangedTask");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
--- CourseWork2/ViewModel/MainVM.cs
+++ CourseWork2/ViewModel/MainVM.cs
@@ -1,7 +1,42 @@
-﻿namespace CourseWork2.ViewModel
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CourseWork2.ViewModel
 {
-    public class MainVM 
+    public class MainVM : INotifyPropertyChanged
     {
-       
+        private DateTime selectedDate;
+        public DateTime SelectedDate
+        {
+            get
+            {
+                return selectedDate;
+            }
+            set
+            {
+                selectedDate = value;
+                OnPropertyChanged("SelectedDate");
+
+            }
+        }
+        public InformationOnDateVM informationOnDateVM
+        {
+            get 
+            { 
+                return informationOnDateVM; 
+            }
+            set
+            {
+                informationOnDateVM = value;
+                OnPropertyChanged("SelectedInformation");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
